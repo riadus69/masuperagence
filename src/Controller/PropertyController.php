c3d9d60764c663c2cb1a16c0967ea286cc57a8b6
Riad SADOK <riad.sadok@orange.com>
2020-10-05 12:17:46 +0200

    #feat/doctrine -creation de table Property -remplissage table Property via les setters

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,19 +3,39 @@
 namespace App\Controller;
 
 
+use App\Entity\Property;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class PropertyController {
+class PropertyController extends AbstractController{
 
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
     public function index(): Response {
-        return new Response('Les biens');
 
+        $property = new Property();
+        $property->setTitle('Mon premier bien')
+            ->setPrice(200000)
+            ->setRooms(4)
+            ->setBedrooms(3)
+            ->setDescription('Une petite description')
+            ->setSurface(60)
+            ->setFloor(4)
+            ->setHeat(1)
+            ->setCity('Lyon')
+            ->setPostalCode(69003);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($property);
+        $em->flush();
+
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties'
+        ]);
     }
 
 }
